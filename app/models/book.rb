cccdf37265d3c4aa65d35140f71bebf8cf60056d
John Cline <jac244@cornell.edu>
2015-01-15 12:49:17 -0500

    tailor for nuremberg

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ActiveFedora::Base
   
   
   has_file_datastream :name=>'digitalImage', :type=>ActiveFedora::Datastream, :mimeType=>"image/jpeg", :controlGroup=>'M', :versionable=>false
+  has_file_datastream :name=>'thumbnailImage', :type=>ActiveFedora::Datastream, :mimeType=>'image/jpeg', :controlGroup=>'M', :versionable=>false
   
   
   belongs_to :derivation, :property=>:has_derivation
@@ -28,6 +29,20 @@ class Book < ActiveFedora::Base
   has_attributes :publisher, datastream: 'descMetadata', multiple: false
   has_attributes :pubplace, datastream: 'descMetadata', multiple: false
   has_attributes :pubdate, datastream: 'descMetadata', multiple: false
+  has_attributes :note, datastream: 'descMetadata', multiple: false
+  has_attributes :vol, datastream: 'descMetadata', multiple: false
+  has_attributes :section, datastream: 'descMetadata', multiple: false
+  has_attributes :dname, datastream: 'descMetadata', multiple: false
+  has_attributes :subdiv, datastream: 'descMetadata', multiple: false
+  has_attributes :part, datastream: 'descMetadata', multiple: false
+  has_attributes :lang, datastream: 'descMetadata', multiple: false
+  has_attributes :author_b, datastream: 'descMetadata', multiple: true
+  has_attributes :witness, datastream: 'descMetadata', multiple: true
+  has_attributes :wit_b, datastream: 'descMetadata', multiple: true
+  has_attributes :onames, datastream: 'descMetadata', multiple: true
+  has_attributes :odates, datastream: 'descMetadata', multiple: true
+  has_attributes :abstract, datastream: 'descMetadata', multiple: false
+  has_attributes :supages, datastream: 'descMetadata', multiple: true
   has_attributes :subject, datastream: 'descMetadata', multiple: true
   has_attributes :editorialdecl_n, datastream: 'descMetadata', multiple: false
   has_attributes :editorialdecl, datastream: 'descMetadata', multiple: false
