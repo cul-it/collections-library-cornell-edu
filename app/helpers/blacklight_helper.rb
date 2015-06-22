594b3be2426232da819b44711b16f6d65e01b7f3
starsplatter <jenncd@gmail.com>
2015-06-22 14:09:30 -0400

    methods for figuring out solr ids, remove page code from search results, methods for previous next within collections

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -23,6 +23,52 @@ def id_to_subject(subject)
   params[:subject] = subject.downcase
 end
 
+def subject_to_prefix(subject)
+  PREFIXES.each do |key,value|
+    if subject == key
+      prefix = value
+      return prefix
+    end
+  end
+  end
+
+def pubstmt_to_id(pubstmt)
+  PREFIXES.each do |key,value|
+    if pubstmt.start_with?(value)
+      prefix = value
+      number = pubstmt.gsub(prefix,':')
+      return prefix + number
+    end
+  end
+end  
+
+# Link to the previous document in the current search context
+  def link_to_previous_document(previous_document)
+    link_opts = session_tracking_params(previous_document, search_session['counter'].to_i - 1).merge(:class => "previous", :rel => 'prev')
+    link_to_unless previous_document.nil?, raw(t('views.pagination.previous')), '/' + params[:subject] +'/' +'catalog' + '/' + previous_document[:id], link_opts do
+      content_tag :span, raw(t('views.pagination.previous')), :class => 'previous'
+    
+    end
+  end
+
+  ##
+  # Link to the next document in the current search context
+  def link_to_next_document(next_document)
+    link_opts = session_tracking_params(next_document, search_session['counter'].to_i + 1).merge(:class => "next", :rel => 'next')
+    link_to_unless next_document.nil?, raw(t('views.pagination.next')), '/'+ params[:subject] +'/' +'catalog' + '/' + next_document[:id], link_opts do
+      content_tag :span, raw(t('views.pagination.next')), :class => 'next'
+    end
+  end
+
+
+PREFIXES = {
+  'scottsboro' => 'scott',
+  'liberian' => 'liber',
+  'trial' => 'sat',
+  'nuremberg' => 'nur'
+}
+
+
   def link_to_document(doc, opts={:label=>nil, :counter => nil, :subject=>params[:subject]})
     opts[:label] ||= document_show_link_field(doc)
     label = render_document_index_label doc, opts
@@ -34,5 +80,8 @@ end
 		link_to label, url_for_document(doc), document_link_params(doc, opts)
 	end
   end
+
+    ##
+
 
 end
