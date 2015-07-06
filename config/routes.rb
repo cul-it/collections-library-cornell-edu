b5e206332dd25cc8b4f528b284d7c31f7a222f56
starsplatter <jenncd@gmail.com>
2015-07-06 11:00:54 -0400

    fix bookmarks links. beautify

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Bl5::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
