bde0d45107f9e290acbfbbac47c6fd443180b2af
jac244 <jac244@cornell.edu>
2015-07-30 09:12:31 -0400

    working on text messaging

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -1,10 +1,10 @@
 # -*- encoding : utf-8 -*-
-# Only works for documents with a #to_marc right now. 
+# Only works for documents with a #to_marc right now.
 class RecordMailer < ActionMailer::Base
   default :from => "culsearch@cornell.edu"
   def email_record(documents, details, url_gen_params)
     #raise ArgumentError.new("RecordMailer#email_record only works with documents with a #to_marc") unless document.respond_to?(:to_marc)
-        
+
 #    subject = I18n.t('blacklight.email.text.subject', :count => documents.length, :title => (documents.first.to_semantic_values[:title] rescue 'N/A') )
     subject = "Item(s) from the Cornell University Law Collections"
     Rails.logger.info("Shrike Documents = #{documents}")
@@ -13,10 +13,10 @@ class RecordMailer < ActionMailer::Base
     @documents      = documents
     @message        = details[:message]
     @url_gen_params = url_gen_params
-    
+
     mail(:to => details[:to],  :subject => subject)
   end
-  
+
   def sms_record(documents, details, url_gen_params)
     if sms_mapping[details[:carrier]]
       to = "#{details[:to]}@#{sms_mapping[details[:carrier]]}"
@@ -30,15 +30,16 @@ class RecordMailer < ActionMailer::Base
   end
 
   protected
-  
+
   def sms_mapping
-    {'virgin' => 'vmobl.com',
-    'att' => 'txt.att.net',
-    'verizon' => 'vtext.com',
-    'nextel' => 'messaging.nextel.com',
-    'sprint' => 'messaging.sprintpcs.com',
-    'tmobile' => 'tmomail.net',
-    'alltel' => 'message.alltel.com',
-    'cricket' => 'mms.mycricket.com'}
+    { 'att' => 'txt.att.net',
+      'alltel' => 'message.alltel.com',
+      'cricket' => 'mms.mycricket.com',
+      'nextel' => 'messaging.nextel.com',
+      'sprint' => 'messaging.sprintpcs.com',
+      'tmobile' => 'tmomail.net',
+      'verizon' => 'vtext.com',
+      'virgin' => 'vmobl.com'
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Bl5::Application.routes.draw do
 
 
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
